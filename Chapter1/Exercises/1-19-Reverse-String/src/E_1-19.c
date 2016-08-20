9dbb173f20f3a8c9f6b855cf089ec98a6df4ad89
Charles Joseph Staal <Charles.Staal@gmail.com>
2016-08-20 14:00:28 -0400

    removed a printf

--- Chapter1/Exercises/1-19-Reverse-String/src/E_1-19.c
+++ Chapter1/Exercises/1-19-Reverse-String/src/E_1-19.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
-int get_array_size(char s[]);
+#define MAXLINE 1000
+
+int getline(char line[]);
+int get_strlen(char s[]);
 void reverse_string(char s[]);
+
 main()
 {
-	char string[] = "StringDingaling";
-	printf("String: %s\n", string);
-	reverse_string(string);
-	printf("Reversed string: %s\n", string);
+	int len;
+	char line[MAXLINE];
+
+	while( (len = getline( line ) ) > 0 )
+	{
+		reverse_string( line );
+		printf( "Reversed string: %s\n", line );
+	}
+
 	return 0;
 }
 
-void reverse_string(char s[])
+void reverse_string( char s[] )
 {
 	char temp;
 	int index0, index1;
 	index0 = 0;
-	index1 = get_array_size(s) - 1;
-	printf( "Array size: %d\n", index1 );
-	for(; index0 < index1; index0++, index1--)
+	index1 = get_strlen( s ) - 1;
+	for( ; index0 < index1; index0++, index1-- )
 	{
 		temp = s[index0];
 		s[index0] = s[index1];
@@ -26,10 +34,26 @@ void reverse_string(char s[])
 	}
 }
 
-int get_array_size(char s[])
+/* getline: reads line into .... line and returns line length*/
+int getline( char line[] )
 {
-	int c = 0;
-	while( s[c] != '\0' )
-		++c;
-	return c;
+	int c, i;
+	int lim = MAXLINE;
+	for( i=0; i < lim - 1 && ( c = getchar() ) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if ( c = '\n' )
+	{
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+int get_strlen( char s[] )
+{
+	int i = 0;
+	while( s[i] != '\0' )
+		i++;
+	return i;
 }

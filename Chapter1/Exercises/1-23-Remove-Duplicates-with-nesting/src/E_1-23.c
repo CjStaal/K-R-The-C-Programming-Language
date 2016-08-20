dc83afa9d9c0507a92d5933fb3071582d29aea19
Charles Joseph Staal <Charles.Staal@gmail.com>
2016-08-20 18:28:43 -0400

    Fixed issue with spaces between comment types creating new lines.

--- Chapter1/Exercises/1-23-Remove-Duplicates-with-nesting/src/E_1-23.c
+++ Chapter1/Exercises/1-23-Remove-Duplicates-with-nesting/src/E_1-23.c
@@ -5,7 +5,7 @@ void remcomments(char s[]);
 int main(int argc, char *argv[])
 {
 	/*ALL THE EDGE CASES!*/
-	char info[] = "\t\t\t\ntest();\n        \ntest2();\n   		\nprintf(/*test*/\"\\\"test\n\t/*iello*/  //more\"/*test*/);/*hello*/ //hello();//test\n/*test*/\ncode(yeah);//test\n//Hello/*hellooo\nmorecode();/*test*/\ncode(/*test*/yeah);/*\n*\n*\n*/\n//\n/*test*/test();\n/*test8*/\nyeah();\n\t\t\ntest();";
+	char info[] = "\t\t\ntest();test2();\n   		\nprintf(/*test*/\"\\\"test\n\t/*iello*/  //more\"/*test*/);/*hello*/ //hello();//test\n/*test*/\ncode(yeah);//test\n//Hello/*hellooo\nmorecode();/*test*/\ncode(/*test*/yeah);/*\n*\n*\n*/\n//\n/*test*/test();\n/*test8*/\nyeah();\n\t\t\ntest();";
 	printf("\n\nTEST:\n%s", info);
 	remcomments(info);
 	printf("\n\nOUTPUT:\n%s", info);
@@ -15,19 +15,17 @@ int main(int argc, char *argv[])
 
 void remcomments(char s[])
 {
-	int slcflag, mlcflag, qflag, pflag, readindex, writeindex, charcount, whitespace;
-	slcflag = mlcflag = qflag = pflag = readindex = writeindex = charcount = whitespace = 0;
+	int slcflag, mlcflag, qflag, scflag, readindex, writeindex, charcount, whitespace;
+	slcflag = mlcflag = qflag = scflag = readindex = writeindex = charcount = whitespace = 0;
 	while (s[readindex] != '\0')
 	{
+		charcount++;
 		if(s[readindex] == ' ' || s[readindex] == '\t' || s[readindex] == '\n')
 			whitespace++;
-		charcount++;
 		if (slcflag == 1 && s[readindex] == '\n')
 			slcflag = 0;
 		if (mlcflag == 1 && s[readindex - 1] == '*' && s[readindex] == '/')
-		{
 			mlcflag = 0;
-		}
 		else if (mlcflag == 0)
 		{
 			if (s[readindex] == '"' && (s[readindex - 1] != '\\' || (s[readindex - 1] == '\\' && s[readindex - 2] == '\\')))
@@ -35,6 +33,8 @@ void remcomments(char s[])
 				else qflag = 0;
 			else if (qflag == 0 && slcflag == 0 && mlcflag == 0)
 			{
+				if(s[readindex - 1] == ';' && (s[readindex] == ' ' || s[readindex] == '\t'))
+					scflag = 1;
 				if (s[readindex] == '/' && s[readindex + 1] == '/')
 				{
 					readindex++;
@@ -45,13 +45,11 @@ void remcomments(char s[])
 					readindex++;
 					mlcflag = 1;
 				}
-			}                                    /*VVV this stops us from reading from a "negative" index and from writing blank lines VVV*/
-			if (slcflag == 0 && mlcflag == 0 && (s[readindex] != '\n' || (s[readindex] == '\n' && writeindex != 0 && s[writeindex - 1] != '\n')))
+				if(s[readindex] == '\n')
+					scflag = 0;
+			}                                                   /*VVV this stops us from reading from a "negative" index and from writing blank lines VVV*/
+			if (scflag == 0 && slcflag == 0 && mlcflag == 0 && (s[readindex] != '\n' || (s[readindex] == '\n' && writeindex != 0 && s[writeindex - 1] != '\n')))
 			{
-				if(s[writeindex - 1] == ';' && s[readindex] != '\n')
-				{
-					s[writeindex++] = '\n';
-				}
 				s[writeindex] = s[readindex];
 				writeindex++;
 			}

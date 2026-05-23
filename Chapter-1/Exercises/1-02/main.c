8955bc4eed895847c53df8fc83c5c4e51c4355c2
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-05-23 17:22:28 -0400

    Added comments on what each do

--- Chapter-1/Exercises/1-02/main.c
+++ Chapter-1/Exercises/1-02/main.c
@@ -2,14 +2,14 @@
 
 int main(void){
 
-    printf("a \a\n");
-    printf("b \b\n");
-    printf("e \e\n");
-    printf("f \f\n");
-    printf("m \m\n");
-    printf("n \n\n");
-    printf("r \r\n");
-    printf("t \t\n");
-    printf("v \v\n");
-    printf("z \z\n");
+    printf("a \a\n");  //alert character
+    printf("b \b\n");  //backspace
+    printf("e \e\n");  //gcc extension
+    printf("f \f\n");  //formfeed
+    printf("m \m\n");  //undefined
+    printf("n \n\n");  //newline
+    printf("r \r\n");  //carriage return
+    printf("t \t\n");  //tab
+    printf("v \v\n");  //vertical tab
+    printf("z \z\n");  //undefined
 }

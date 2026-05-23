ca299b1542a50b5499aaef9300681ceae9bea2b4
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-05-23 17:09:18 -0400

    Removed redundant check

--- Chapter-1/Exercises/1-12/main.c
+++ Chapter-1/Exercises/1-12/main.c
@@ -8,18 +8,16 @@ int main(void){
     int state = OUT_WORD;
     int current_character = 0;
     while((current_character = getchar()) != EOF){
-        if(current_character != EOF){
-            if(current_character == ' ' || current_character == '\n' || current_character == '\t'){
-                if(state == IN_WORD){
-                    printf("\n");
-                }
-                state = OUT_WORD;
+        if(current_character == ' ' || current_character == '\n' || current_character == '\t'){
+            if(state == IN_WORD){
+                printf("\n");
             }
-            else{
-                state = IN_WORD;
-                putchar(current_character);
-            }
+            state = OUT_WORD;
         }
-    } 
+        else{
+            state = IN_WORD;
+            putchar(current_character);
+        }
+    }
     return 0;
 }

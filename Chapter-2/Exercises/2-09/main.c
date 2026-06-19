b5e35e6f7bf096ed1dddb02f747e27e51c7e908d
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-06-18 21:14:44 -0400

    Created file

---
+++ Chapter-2/Exercises/2-09/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+    In a two's complement number system, x &= (x-1) deletes the
+    rightmost 1-bit in x. Explain why. Use this observation to
+    write a faster version of bitcount.
+*/
+
+int main(){
+
+    return 0;
+}
---
+++ Chapter-2/Exercises/2-10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+    Rewrite the function lower, which converts upper case letters
+    to lower case, with a condition expression instead of if-else.
+*/
+
+int main(){
+
+    return 0;
+}

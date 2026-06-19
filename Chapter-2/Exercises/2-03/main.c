dc15c0c9f5cc2d7c71a12f2cd7c8b54c1e6df504
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-06-18 20:19:59 -0400

    fixed typo

--- Chapter-2/Exercises/2-03/main.c
+++ Chapter-2/Exercises/2-03/main.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 /*
     Exercise 2-03
-    Write the function htoi(s), which conerts a string of hexadecimal digits
+    Write the function htoi(s), which converts a string of hexadecimal digits
     (including an optional 0x or 0X) into its equivalent integer value.
     The allowable digits are 0 through 9, a through f, and A through F.
 */

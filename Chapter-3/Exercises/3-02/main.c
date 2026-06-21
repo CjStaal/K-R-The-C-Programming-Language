1c3288627b6c0b475b1b9eb0856fd4f4636c0d2d
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-06-20 22:25:32 -0400

    Finished 3-02

---
+++ Chapter-3/Exercises/3-02/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+/*
+    Write a function escape(s,t) that converts characters like newline and
+    tab into visible escape sequences like \n and \t as it copies the string
+    t to s. Use a switch. Write a function for the other direction as well,
+    converting escape sequences into the real characters.
+*/
+
+void escape(char[], char[]);
+void unescape(char[], char[]);
+
+int main(){
+
+    return 0;
+}
+
+void escape(char s[], char t[]){
+    
+    int read = 0, write = 0;
+    for(read = 0, write = 0; t[read] != '\0'; read++){
+        switch(t[read]){
+            case '\n':
+                s[write++] = '\\';
+                s[write++] = 'n';
+                break;
+            case '\t':
+                s[write++] = '\\';
+                s[write++] = 't';
+                break;
+            default:
+                s[write++] = t[read];
+                break;
+        }
+    }
+    s[write]= '\0';
+}
+
+void unescape(char s[], char t[]){
+    
+    int read = 0, write = 0;
+    for(read = 0, write = 0; t[read] != '\0'; read++){
+        switch(t[read]){
+            case '\\':
+                switch(t[read+1]){
+                    case 'n':
+                        s[write++] = '\n';
+                        read++;
+                        break;
+                    case 't':
+                        s[write++] = '\t';
+                        read++;
+                        break;
+                    default:
+                        s[write++] = t[read];
+                        break;
+                }
+                break;
+            default:
+                s[write++] = t[read];
+                break;
+        }
+    }
+    s[write]= '\0';
+}

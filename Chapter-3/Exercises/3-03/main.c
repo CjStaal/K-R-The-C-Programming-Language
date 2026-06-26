f2226d5cefa57c62f4c9a41039127e4f68ce759e
Charles Joseph Staal <Charles.Staal@gmail.com>
2026-06-25 22:05:14 -0400

    Started 3-03

--- Chapter-3/Exercises/3-03/main.c
+++ Chapter-3/Exercises/3-03/main.c
@@ -8,8 +8,44 @@
     and a-z0-9 and -a-z. Arrange that a leading or trailing - is
     taken literally.
 */
+#define MAXLINE 1000
+
+void expand(const char[], char[]);
 
 int main(){
 
+    char *testcase[] = {
+        "a-z",
+        "a-z0-9",
+        "a-b-c",
+        "-a-z",
+        "a-z",
+        "-a-z-",
+        "a-Z",
+        "A-Z"
+    };
+    char output[MAXLINE];
+
+    int index = 0, count = sizeof(testcase)/sizeof(testcase[0]);
+    for(index = 0; index < count; index++){
+        expand(testcase[index], output);
+        printf("%s -> %s\n", testcase[index], output);
+    }
+
     return 0;
+}
+
+void expand(const char inputline[], char outputline[]){
+    int read = 0, write = 0;
+    while (inputline[read] != '\0'){
+        if((read > 0 && inputline[read+1] != '\0') && inputline[read] == '-'){
+
+        }
+        else{
+            outputline[write++] = inputline[read++];
+        }
+    }
+
+    outputline[write] = '\0';
+
 }
